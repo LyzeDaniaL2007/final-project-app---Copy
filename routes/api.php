8c34574417f375b6e41beec19c1f3047d79e8d1b
LyzeDaniaL2007 <ytdanial07@gmail.com>
2025-02-11 19:23:48 +0700

    Change Route v1

--- routes/api.php
+++ routes/api.php
@@ -22,62 +22,63 @@ Route::get('/user', function (Request $request) {
 
 
 //Auth
-Route::post('/admin/auth/login', [AuthController::class, 'login']);
-Route::post('/admin/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/register', [AuthController::class, 'register']);
 Route::middleware('auth:api')->post('/refresh-password', [AuthController::class, 'refreshPassword']);
 
 //Admin
-Route::prefix('admin')->group(function () {
-    Route::get('/', [AdminController::class, 'index']);
-    Route::get('/{id}', [AdminController::class, 'show']);
-    Route::post('/', [AdminController::class, 'store']);
-    Route::put('/{id}', [AdminController::class, 'update']);
-    Route::delete('/{id}', [AdminController::class, 'destroy']);
-    Route::put('/up/{id}', [AdminController::class, 'update_pass']);
-
+Route::prefix('v1')->group(function () {
+    Route::prefix('admin')->group(function () {
+        Route::get('/', [AdminController::class, 'index']);
+        Route::get('/{id}', [AdminController::class, 'show']);
+        Route::post('/', [AdminController::class, 'store']);
+        Route::put('/{id}', [AdminController::class, 'update']);
+        Route::delete('/{id}', [AdminController::class, 'destroy']);
+        Route::put('/up/{id}', [AdminController::class, 'update_pass']);
+    });
 });
 
 //pelanggan
-Route::get('/pelanggan', [PelangganController::class, 'index']);
-Route::get('/pelanggan/{pelanggan_id}', [PelangganController::class, 'show']);
-Route::post('/pelanggan', [PelangganController::class, 'store']);
-Route::put('/pelanggan/{pelanggan_id}', [PelangganController::class, 'update']);
-Route::delete('/pelanggan/{pelanggan_id}', [PelangganController::class, 'destroy']);
+Route::get('/v1/v1/pelanggan', [PelangganController::class, 'index']);
+Route::get('/v1/pelanggan/{pelanggan_id}', [PelangganController::class, 'show']);
+Route::post('/v1/pelanggan', [PelangganController::class, 'store']);
+Route::put('/v1/pelanggan/{pelanggan_id}', [PelangganController::class, 'update']);
+Route::delete('/v1/pelanggan/{pelanggan_id}', [PelangganController::class, 'destroy']);
 
 //pelangganData
-Route::get('/pelanggan-data', [PelangganDataController::class, 'index']);
-Route::get('/pelanggan-data/{id}', [PelangganDataController::class, 'show']);
-Route::post('/pelanggan-data', [PelangganDataController::class, 'store']);
-Route::put('/pelanggan-data/{id}', [PelangganDataController::class, 'update']);
-Route::delete('/pelanggan-data/{id}', [PelangganDataController::class, 'destroy']);
+Route::get('/v1/data/pelanggan', [PelangganDataController::class, 'index']);
+Route::get('/v1/data/pelanggan/{id}', [PelangganDataController::class, 'show']);
+Route::post('/v1/data/pelanggan', [PelangganDataController::class, 'store']);
+Route::put('/v1/data/pelanggan/{id}', [PelangganDataController::class, 'update']);
+Route::delete('/v1/data/pelanggan/{id}', [PelangganDataController::class, 'destroy']);
 
 //penyewaan
-Route::get('/penyewaan', [PenyewaanController::class, 'index']);
-Route::get('/penyewaan/{id}', [PenyewaanController::class, 'show']);
-Route::post('/penyewaan', [PenyewaanController::class, 'store']);
-Route::put('/penyewaan/{id}', [PenyewaanController::class, 'update']);
-Route::delete('/penyewaan/{id}', [PenyewaanController::class, 'destroy']);
+Route::get('/v1/penyewaan', [PenyewaanController::class, 'index']);
+Route::get('/v1/penyewaan/{id}', [PenyewaanController::class, 'show']);
+Route::post('/v1/penyewaan', [PenyewaanController::class, 'store']);
+Route::put('/v1/penyewaan/{id}', [PenyewaanController::class, 'update']);
+Route::delete('/v1/penyewaan/{id}', [PenyewaanController::class, 'destroy']);
 
-//Kategori
-Route::get('/kategori', [KategoriController::class, 'index']);
-Route::get('/kategori/{id}', [KategoriController::class, 'show']);
-Route::post('/kategori', [KategoriController::class, 'store']);
-Route::put('/kategori/{id}', [KategoriController::class, 'update']);
-Route::delete('/kategori/{id}', [KategoriController::class, 'destroy']);
+//kategori
+Route::get('/v1/kategori', [KategoriController::class, 'index']);
+Route::get('/v1/kategori/{id}', [KategoriController::class, 'show']);
+Route::post('/v1/kategori', [KategoriController::class, 'store']);
+Route::put('/v1/kategori/{id}', [KategoriController::class, 'update']);
+Route::delete('/v1/kategori/{id}', [KategoriController::class, 'destroy']);
 
-//Alat
-Route::get('/alat', [AlatController::class, 'index']);
-Route::get('/alat/{id}', [AlatController::class, 'show']);
-Route::post('/alat', [AlatController::class, 'store']);
-Route::put('/alat/{id}', [AlatController::class, 'update']);
-Route::delete('/alat/{id}', [AlatController::class, 'destroy']);
+//alat
+Route::get('/v1/alat', [AlatController::class, 'index']);
+Route::get('/v1/alat/{id}', [AlatController::class, 'show']);
+Route::post('/v1/alat', [AlatController::class, 'store']);
+Route::put('/v1/alat/{id}', [AlatController::class, 'update']);
+Route::delete('/v1/alat/{id}', [AlatController::class, 'destroy']);
 
-//PenyewaanDetail
-Route::get('/penyewaan-detail', [PenyewaanDetailController::class, 'index']);
-Route::get('/penyewaan-detail/{id}', [PenyewaanDetailController::class, 'show']);
-Route::post('/penyewaan-detail', [PenyewaanDetailController::class, 'store']);
-Route::put('/penyewaan-detail/{id}', [PenyewaanDetailController::class, 'update']);
-Route::delete('/penyewaan-detail/{id}', [PenyewaanDetailController::class, 'destroy']);
+//penyewaanDetail
+Route::get('/v1/detail/penyewaan', [PenyewaanDetailController::class, 'index']);
+Route::get('/v1/detail/penyewaan/{id}', [PenyewaanDetailController::class, 'show']);
+Route::post('/v1/detail/penyewaan', [PenyewaanDetailController::class, 'store']);
+Route::put('/v1/detail/penyewaan/{id}', [PenyewaanDetailController::class, 'update']);
+Route::delete('/v1/detail/penyewaan/{id}', [PenyewaanDetailController::class, 'destroy']);
 
 Route::group(['middleware' => 'auth:api'], function () {
     Route::apiResource('admin', AdminController::class);
